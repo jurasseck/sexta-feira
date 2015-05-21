e74bb06b9ce4a964b6770ca1c05c9558627f890b
Luis Henrique Jurasseck <luis.jurasseck@gmail.com>
2015-05-21 13:46:41 -0300

    ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,25 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['client/js/app.js','client/js/controllers.js','client/js/services.js'],
+        dest: 'dist/build.js',
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/build.min.js': ['dist/build.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
